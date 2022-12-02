c9b637099468e5c5fb408e306a84d26c2f4a91a1
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-12-02 17:03:42 +0100

    1st iteration. buttons for grid size

--- script.js
+++ script.js
@@ -1,22 +1,57 @@
-//Variables
+
+// VARIABLES
 const mainContainerEl = document.querySelector('.mainContainer');
-const gridSizeEl = document.querySelector('.gridSize');
+const buttons = document.querySelectorAll('.gridSizeButton');
+let gridSizeEl = 5;
 
-// Styling
-mainContainerEl.style.setProperty('grid-template-columns', `repeat(${gridSizeEl.textContent}, 1fr)`);
-mainContainerEl.style.setProperty('grid-template-rows', `repeat(${gridSizeEl.textContent}, 1fr)`);
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+    // and for each one we add a 'click' listener
+    button.addEventListener('click', () => {
+        if (button.id == 'small') {
+            gridSizeEl = 5;
+            console.log(gridSizeEl);
+            creatingGrid();
+        } else if (button.id == 'medium') {
+            gridSizeEl = 15;
+            console.log(gridSizeEl);
+            creatingGrid();
+        } else if (button.id == 'large') {
+            gridSizeEl = 30;
+            console.log(gridSizeEl);
+            creatingGrid();
+        }
+    });
+});
 
-// GRID Function
+// STYLING
+mainContainerEl.style.setProperty('grid-template-columns', `repeat(${gridSizeEl}, 1fr)`);
+mainContainerEl.style.setProperty('grid-template-rows', `repeat(${gridSizeEl}, 1fr)`);
+
+// FUNCTIONS
 function createGrid(val) {
     const squareEl = document.createElement('div');
-    squareEl.textContent = val
+    squareEl.classList.add('boxes');
+    squareEl.textContent = val;
     return squareEl;
 }
 
-//GRID Creation
-for (let index = 0; index < (gridSizeEl.textContent * gridSizeEl.textContent); index++) {
-    mainContainerEl.appendChild(createGrid(index + 1));
+// CLEAR GRID
+function emptyGrid() {
+    console.log('boxes');
+    mainContainerEl.textContent = '';
 }
 
-console.log('1 mainContainerEl.className' + mainContainerEl.className);
-console.log('2 gridSizeEl.textContent ' + gridSizeEl.textContent);
+// INITIAL GRID (Needed to display an initial GRID)
+creatingGrid();
+
+// GRID CREATION
+function creatingGrid() {
+    emptyGrid();
+    for (let index = 0; index < (gridSizeEl * gridSizeEl); index++) {
+        mainContainerEl.appendChild(createGrid(index + 1));
+    }
+}
+
+// OTHER
+console.log('INITIAL gridSizeEl ' + gridSizeEl);
