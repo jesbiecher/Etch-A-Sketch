c69902c187210db8fa128e79dd7acb48114bad60
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-12-02 15:44:23 +0100

    1st iteration. basic grid fixes

--- script.js
+++ script.js
@@ -2,29 +2,21 @@
 const mainContainerEl = document.querySelector('.mainContainer');
 const gridSizeEl = document.querySelector('.gridSize');
 
-const createGridEl = document.querySelector('.createGrid');
-
-// Event Listeners
-createGridEl.addEventListener('click', startGame);
-
 // Styling
-mainContainerEl.style.setProperty('grid-template-columns', `repeat(${gridSizeEl.value}, 1fr)`);
-mainContainerEl.style.setProperty('grid-template-rows', `repeat(${gridSizeEl.value}, 1fr)`);
-
-function startGame(e) {
-    e.preventDefault();
-    console.log(gridSizeEl.value);
-    createGrid(gridSizeEl.value);
-}
+mainContainerEl.style.setProperty('grid-template-columns', `repeat(${gridSizeEl.textContent}, 1fr)`);
+mainContainerEl.style.setProperty('grid-template-rows', `repeat(${gridSizeEl.textContent}, 1fr)`);
 
 // GRID Function
 function createGrid(val) {
     const squareEl = document.createElement('div');
-    squareEl.textContent = val;
-    return squareEl.textContent;
+    squareEl.textContent = val
+    return squareEl;
 }
 
 //GRID Creation
-for (let index = 0; index < (gridSizeEl.value * gridSizeEl.value); index++) {
+for (let index = 0; index < (gridSizeEl.textContent * gridSizeEl.textContent); index++) {
     mainContainerEl.appendChild(createGrid(index + 1));
 }
+
+console.log('1 mainContainerEl.className' + mainContainerEl.className);
+console.log('2 gridSizeEl.textContent ' + gridSizeEl.textContent);
