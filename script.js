634f6a15ff3c2c5bb0eed7e2c50a8385da72ef38
Jesica Biecher <j.biecher@fvfj92d21wgc.speedport.ip>
2022-12-02 19:15:40 +0100

    1st iteration. small changes to grid

--- script.js
+++ script.js
@@ -2,43 +2,45 @@
 // VARIABLES
 const mainContainerEl = document.querySelector('.mainContainer');
 const buttons = document.querySelectorAll('.gridSizeButton');
-let gridSizeEl = 5;
+let gridSizeEl = 10;
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
     // and for each one we add a 'click' listener
     button.addEventListener('click', () => {
-        if (button.id == 'small') {
-            gridSizeEl = 5;
+        if (button.id == 'xSmall') {
+            gridSizeEl = 10;
+            console.log(gridSizeEl);
+            creatingGrid();
+        } else if (button.id == 'small') {
+            gridSizeEl = 20;
             console.log(gridSizeEl);
             creatingGrid();
         } else if (button.id == 'medium') {
-            gridSizeEl = 15;
+            gridSizeEl = 30;
             console.log(gridSizeEl);
             creatingGrid();
         } else if (button.id == 'large') {
-            gridSizeEl = 30;
+            gridSizeEl = 40;
+            console.log(gridSizeEl);
+            creatingGrid();
+        } else if (button.id == 'xLarge') {
+            gridSizeEl = 50;
             console.log(gridSizeEl);
             creatingGrid();
         }
     });
 });
 
-// STYLING
-mainContainerEl.style.setProperty('grid-template-columns', `repeat(${gridSizeEl}, 1fr)`);
-mainContainerEl.style.setProperty('grid-template-rows', `repeat(${gridSizeEl}, 1fr)`);
-
 // FUNCTIONS
 function createGrid(val) {
     const squareEl = document.createElement('div');
-    squareEl.classList.add('boxes');
-    squareEl.textContent = val;
+    squareEl.classList.add('box');
     return squareEl;
 }
 
 // CLEAR GRID
 function emptyGrid() {
-    console.log('boxes');
     mainContainerEl.textContent = '';
 }
 
@@ -47,11 +49,22 @@ creatingGrid();
 
 // GRID CREATION
 function creatingGrid() {
+    // STYLING
+    mainContainerEl.style.setProperty('grid-template-columns', `repeat(${gridSizeEl}, 1fr)`);
+    mainContainerEl.style.setProperty('grid-template-rows', `repeat(${gridSizeEl}, 1fr)`);
+
     emptyGrid();
     for (let index = 0; index < (gridSizeEl * gridSizeEl); index++) {
         mainContainerEl.appendChild(createGrid(index + 1));
     }
 }
 
-// OTHER
-console.log('INITIAL gridSizeEl ' + gridSizeEl);
+//MOUSEOVER
+function mouseOver() {
+    document.querySelector(".gridSizeButton").style.color = "red";
+}
+
+//MOUSEOVER
+function mouseOut() {
+    document.querySelector(".gridSizeButton").style.color = "black";
+}
